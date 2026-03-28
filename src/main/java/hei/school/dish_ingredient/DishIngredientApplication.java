ae6517d341d679a5d8588b9b99c3b3cc57bf4ed5
Vaniaina <vanimendes344@gmail.com>
2026-03-28 19:27:35 +0300

    project initialization

---
+++ src/main/java/hei/school/dish_ingredient/DishIngredientApplication.java
@@ -0,0 +1,13 @@
+package hei.school.dish_ingredient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishIngredientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DishIngredientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hei/school/dish_ingredient/DishIngredientApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.school.dish_ingredient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishIngredientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
