7cabc4be70499cabda2fbfb32e31f0f608a65a3c
Vaniaina <vanimendes344@gmail.com>
2026-03-29 18:31:52 +0300

    feat: add createIngredients method to the ingredientRepository

--- src/main/java/hei/school/dish_ingredient/repository/IngredientRepository.java
+++ src/main/java/hei/school/dish_ingredient/repository/IngredientRepository.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.sql.DataSource;
 
@@ -43,6 +44,38 @@ public class IngredientRepository {
                 return ingredient;
             }
             return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Ingredient> createIngredients(List<Ingredient> newIngredients) {
+        if (newIngredients == null || newIngredients.isEmpty()) {
+            return List.of();
+        }
+
+        String query = "INSERT INTO ingredient (name, price, category) VALUES (?, ?, ?)";
+        try (Connection conn = dataSource.getConnection();
+                PreparedStatement ps = conn.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS)) {
+
+            conn.setAutoCommit(false);
+
+            for (Ingredient ingredient : newIngredients) {
+                ps.setString(1, ingredient.getName());
+                ps.setDouble(2, ingredient.getPrice());
+                ps.setString(3, ingredient.getCategory().name());
+                ps.addBatch();
+            }
+            ps.executeBatch();
+
+            ResultSet rs = ps.getGeneratedKeys();
+
+            List<Ingredient> ingredients = newIngredients;
+            int index = 0;
+            while (rs.next()) {
+                ingredients.get(index++).setId(rs.getInt(1));
+            }
+            return ingredients;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
